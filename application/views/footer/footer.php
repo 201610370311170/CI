b655eff98b6436a830e91924cedcf74df1b83479
Yudi <yudia20.ya@gmail.com>
2019-10-14 20:54:46 +0700

    JavaScript

--- application/views/footer/footer.php
+++ application/views/footer/footer.php
@@ -19,20 +19,27 @@
 <script src="<?php echo base_url('assets/js/jquery.slicknav.min.js') ?>"></script>
 
 <!-- start chart js -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+
+<script src="<?php echo base_url('assets/js/Chart.min.js') ?>"></script>
+
 <!-- start highcharts js -->
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
-<script src="https://code.highcharts.com/modules/export-data.js"></script>
+
+<script src="<?php echo base_url('assets/js/highcharts.js') ?>"></script>
+<script src="<?php echo base_url('assets/js/exporting.js') ?>"></script>
+<script src="<?php echo base_url('assets/js/export-data.js') ?>"></script>
+
 <!-- start amcharts -->
-<script src="https://www.amcharts.com/lib/3/amcharts.js"></script>
-<script src="https://www.amcharts.com/lib/3/ammap.js"></script>
-<script src="https://www.amcharts.com/lib/3/maps/js/worldLow.js"></script>
-<script src="https://www.amcharts.com/lib/3/serial.js"></script>
-<script src="https://www.amcharts.com/lib/3/plugins/export/export.min.js"></script>
-<script src="https://www.amcharts.com/lib/3/themes/light.js"></script>
+
+<script src="<?php echo base_url('assets/js/amcharts.js') ?>"></script>
+<script src="<?php echo base_url('assets/js/ammap.js') ?>"></script>
+<script src="<?php echo base_url('assets/js/worldLow.js') ?>"></script>
+<script src="<?php echo base_url('assets/js/serial.js') ?>"></script>
+<script src="<?php echo base_url('assets/js/export.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/js/light.js') ?>"></script>
+
 <!-- all line chart activation -->
 <script src="<?php echo base_url('assets/js/line-chart.js') ?>"></script>
+
 <!-- all pie chart -->
 <script src="<?php echo base_url('assets/js/pie-chart.js') ?>"></script>
 <!-- all bar chart -->
